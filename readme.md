60830c75de78cadccee62bd43436adeb64d18be2
Salim Sadman <mohammadfahim911@gmail.com>
2020-07-23 01:34:36 +0600

    skeleton upgrade

--- readme.md
+++ readme.md
@@ -2,3 +2,5 @@ etch a sketch
 made according to TOP
 
 my initial plan to to add nothing to the html file except the source links . I want to use DOM to design the whole page . This is my hardest project so far , and i c an say i've done enough . I'm startin to write the code on 22th july at 1:30 pm , lets see how long it takes. 
+
+its exactly 1:30 am and i've finsihed the project ! or at least i've finished the main skeleton , the styling is still left . This seemed overwhelming at first but it turned out way easier than i thought . Maybe it's just miscalculation or maybe i've actually become a better coder :D
---
+++ script.js
@@ -0,0 +1,87 @@
+//element creation
+const head = document.createElement(`header`);
+//const main = document.createElement(`main`);
+const title = document.createElement('h1');
+const des = document.createElement(`section`);
+const button = document.createElement(`button`);
+const p = document.createElement(`p`);
+
+
+//appending children
+document.body.appendChild(head);
+//document.body.appendChild(main);
+head.appendChild(title);
+head.appendChild(des);
+head.appendChild(button);
+head.appendChild(p);
+
+//attributes and content
+head.setAttribute('id','header');
+//main.setAttribute(`id`,`main`);
+
+des.textContent = `  hover over the grids to change the color and draw stuff !  `;
+title.textContent = `Etch A Sketch`;
+button.textContent = `ooh click me click me`
+
+
+
+
+
+
+function setGrids () {
+ let gridList = document.querySelectorAll("div");
+ gridList.forEach(function (e) {
+     e.classList.add('gridstyle');
+     //console.log(e);
+    e.addEventListener('mouseover', function(element) {
+    			console.log(element);
+    			e.classList.add(`hoverBlack`);
+    });
+    e.addEventListener('click', function(element) {
+    			console.log(element);
+    			e.classList.remove(`hoverBlack`);
+    });
+ }); 
+ button.onclick = function () { 
+ 	gridList.forEach( function (e) { e.classList.remove(`hoverBlack`) })
+ 	this.textContent = `i clear the sketch `;
+ 	return 0;	
+ };
+ return 0;
+}
+
+
+
+function grids (count) {
+	let gridsArr = []
+	const main = document.createElement(`main`);
+	document.body.appendChild(main);
+	main.setAttribute(`id`,`main`);
+
+	for (let i = count*count; i > 0; i--) {
+		gridsArr.push(document.createElement(`div`));
+	}
+	gridsArr.forEach(e => main.appendChild(e));
+	setGrids();
+	main.style.cssText = `grid-template-columns: repeat(${count},auto); grid-template-rows: repeat(${count},auto);  grid-column-gap:${72/count}px;
+  	grid-row-gap: ${72/count}px; border:${18/count}px solid bisque`;
+  	p.innerHTML = `grid size : ${count} x ${count} `;
+	
+	return 0;
+}
+grids(16);
+
+p.addEventListener(`click`,function (){
+	let input = window.prompt(`please input new grid size`);
+	document.body.removeChild(main);
+	grids(input);
+
+});
+
+
+/*
+window.addEventListener('keydown',function (key){
+	console.log(e);
+	gridList.forEach(function (e) {
+		e.style.backgroundColor = `red`;
+})};*/
